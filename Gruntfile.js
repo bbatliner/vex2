978d611a8025d71dd127fef73cb0cc1ffdf9fa39
Brendan Batliner <brendan.batliner@gmail.com>
2016-07-17 00:41:36 -0500

    Added combined build with vex2-dialog

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,21 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    dialogPkg: grunt.file.readJSON('node_modules/vex2-dialog/package.json'),
 
     browserify: {
       vex: {
         src: 'src/vex2.js',
         dest: 'dist/vex2.js',
+        options: {
+          browserifyOptions: {
+            'standalone': 'vex'
+          }
+        }
+      },
+      combined: {
+        src: 'src/vex2.combined.js',
+        dest: 'dist/vex2.combined.js',
         options: {
           browserifyOptions: {
             'standalone': 'vex'
@@ -20,6 +30,14 @@ module.exports = function (grunt) {
         dest: 'dist/vex2.min.js',
         options: {
           banner: '/*! vex2.js <%= pkg.version %> */\n',
+          report: 'gzip'
+        }
+      },
+      combined: {
+        src: 'dist/vex2.combined.js',
+        dest: 'dist/vex2.combined.min.js',
+        options: {
+          banner: '/*! vex2.combined.js: vex2 <%= pkg.version %>, vex2-dialog <%= dialogPkg.version %> */\n',
           report: 'gzip'
         }
       }
---
+++ src/vex2.combined.js
@@ -0,0 +1,3 @@
+var vex = require('./vex2')
+vex.registerPlugin(require('vex2-dialog'))
+module.exports = vex
